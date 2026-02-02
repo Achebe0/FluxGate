7755a5a73c01242dd84ad884f16c0ae3ee814f1c
Achebe <achebeculpepper@gmail.com>
2026-02-01 23:06:20 -0500

    added MockBean for the test application

--- src/main/java/com/learnspring/fluxgate/FluxGateApplication.java
+++ src/main/java/com/learnspring/fluxgate/FluxGateApplication.java
@@ -3,12 +3,7 @@ package com.learnspring.fluxgate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-        exclude = {
-                org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-                org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-        }
-)
+@SpringBootApplication
 public class FluxGateApplication {
     public static void main(String[] args) {
         SpringApplication.run(FluxGateApplication.class, args);
--- src/test/java/com/learnspring/fluxgate/FluxGateApplicationTests.java
+++ src/test/java/com/learnspring/fluxgate/FluxGateApplicationTests.java
@@ -8,8 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 //@SpringBootTest
 class FluxGateApplicationTests {
 
-    @MockBean
-    private PromptLogRepository promptLogRepository;
+
 
     @Test
     void contextLoads() {
