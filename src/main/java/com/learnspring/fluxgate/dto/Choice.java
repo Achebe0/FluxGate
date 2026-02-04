db5758c6f11a289ea2b2f02493830bc6e2dbfca4
Achebe <achebeculpepper@gmail.com>
2026-02-04 01:59:16 -0500

    included delta feature

--- src/main/java/com/learnspring/fluxgate/dto/Choice.java
+++ src/main/java/com/learnspring/fluxgate/dto/Choice.java
@@ -1,5 +1,9 @@
 package com.learnspring.fluxgate.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record Choice(
-    Message message
+    Message message,
+    Message delta
 ) {}
